68c7a26cff07c93e17aad35884b52eb77cd8fabf
DimzDev <masdimaz.2010@gmail.com>
2026-09-21 22:04:17 +0700

    Add files via upload

---
+++ mesin-penentu_umur.py
@@ -0,0 +1,15 @@
+#mesin penentu titit
+
+Nama = input("masukan nama: ")
+Umur = int(input("masukan umur: "))
+
+print("===============================")
+print("halooo", Nama)
+print("umur kamu sekarang", Umur)
+a = Umur + 5
+b = Umur + 10
+
+print("5 tahun lagi umurmu: ", a)
+print("10 tahun lagi umurmu: ", b)
+print("===============================")
+

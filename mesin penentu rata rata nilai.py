7ab9dcd72f740cf0848c4f58392d2b7510a642b2
DimzDev <masdimaz.2010@gmail.com>
2026-09-21 21:40:37 +0700

    Add files via upload

---
+++ mesin penentu rata rata nilai.py
@@ -0,0 +1,14 @@
+print("====== PENENTU NILAI ======")
+nama = input("masukan nama: ")
+mtk = int(input("masukan nilai matematika: "))
+indo = int(input("masukan nilai b.indo: "))
+ing = int(input("masukan nilai b.inggris: "))
+
+print("=====================================")
+print("Nama: ", nama)
+nilai = mtk + indo + ing
+print("Total nilai: ", nilai)
+bagi = float(nilai/3)
+print("Rata rata nilai: ", bagi)
+print("terimakasih sudah menggunakan program ini!")
+print("====================================")
